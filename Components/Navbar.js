e67c4a9f7c03c9bd49245b9f367a560e8509d3e7
sleepinbliss <160824699+gr00tisg00d@users.noreply.github.com>
2026-02-22 13:42:00 -0600

    [1.15] Added VueJs To the Project. Made the Navbar into a component file that is used in index.html

---
+++ Components/Navbar.js
@@ -0,0 +1,21 @@
+export default {
+    template: `
+            <nav class="navbar navbar-expand-lg bg-dark px-3 row w-fit-content m-auto rounded-4">
+            <div class="col-12 d-flex gap-4 justify-content-center ">
+                <a href="/" class="icon-btn d-flex align-items-center justify-content-center text-decoration-none">
+                    <i class="bi bi-bug-fill fs-2 d-flex align-items-center"></i>
+                </a>
+                <a href="/" class="icon-btn d-flex align-items-center justify-content-center text-decoration-none">
+                    <i class="bi bi-bar-chart-fill fs-2 d-flex align-items-center"></i>
+                </a>
+                <a href="/" class="icon-btn d-flex align-items-center justify-content-center text-decoration-none">
+                    <i class="bi bi-person-circle fs-2 d-flex align-items-center"></i>
+                </a>
+                <a href="/"
+                    class="icon-btn d-flex align-items-center justify-content-center active-custom text-decoration-none">
+                    <i class="bi bi-coin fs-2 d-flex align-items-center"></i>
+                </a>
+            </div>
+        </nav>
+    `
+};
---
+++ app.js
@@ -0,0 +1,8 @@
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+import Navbar from "./Components/Navbar.js";
+
+createApp({
+    components:
+        { Navbar },
+
+}).mount('#app');
